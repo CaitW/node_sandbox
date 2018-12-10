9a26f918832a3d7755f3a5624e5805a57eccb7e9
Caitlin <wolters.caitlin@gmail.com>
2018-12-10 10:08:48 -0500

    diff output

---
+++ knock/myOutput.js
@@ -0,0 +1,113 @@
+[
+  [
+    [
+      {
+        "homeId": 413,
+        "city": "New York",
+        "neighborhood": "Crown Heights"
+      }
+    ],
+    [
+      {
+        "homeId": 411,
+        "city": "New York",
+        "neighborhood": "Melrose"
+      }
+    ],
+    [
+      {
+        "homeId": 412,
+        "city": "New York",
+        "neighborhood": "Williamsburg"
+      }
+    ],
+    [
+      {
+        "homeId": 212,
+        "city": "Boston",
+        "neighborhood": "Back Bay"
+      }
+    ],
+    [
+      {
+        "homeId": 211,
+        "city": "Boston",
+        "neighborhood": "Charlestown"
+      }
+    ]
+  ],
+  [
+    [
+      {
+        "homeId": 213,
+        "city": "Boston",
+        "neighborhood": "Dudley Square"
+      }
+    ],
+    [
+      {
+        "homeId": 313,
+        "city": "Chicago",
+        "neighborhood": "Avondale"
+      }
+    ],
+    [
+      {
+        "homeId": 312,
+        "city": "Chicago",
+        "neighborhood": "Hyde Park"
+      }
+    ],
+    [
+      {
+        "homeId": 311,
+        "city": "Chicago",
+        "neighborhood": "Wrigleyville"
+      }
+    ],
+    [
+      {
+        "homeId": 11,
+        "city": "Atlanta",
+        "neighborhood": "Buckhead"
+      }
+    ]
+  ],
+  [
+    [
+      {
+        "homeId": 12,
+        "city": "Atlanta",
+        "neighborhood": "Inman Park"
+      }
+    ],
+    [
+      {
+        "homeId": 13,
+        "city": "Atlanta",
+        "neighborhood": "Lakewood"
+      }
+    ],
+    [
+      {
+        "homeId": 111,
+        "city": "Nashville",
+        "neighborhood": "Brentwood"
+      }
+    ],
+    [
+      {
+        "homeId": 112,
+        "city": "Nashville",
+        "neighborhood": "Buena Vista"
+      }
+    ],
+    [
+      {
+        "homeId": 113,
+        "city": "Nashville",
+        "neighborhood": "Germantown"
+      }
+    ]
+  ]
+]
