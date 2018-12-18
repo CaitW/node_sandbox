7e82e99dfe9fe6c7ed55ac517ab2e437e04c1a11
Caitlin <wolters.caitlin@gmail.com>
2018-12-18 11:02:27 -0500

    flag-i18n

---
+++ flag-i18n/index.js
@@ -0,0 +1,38 @@
+// metadataService.metadata_descriptions.+?(?=\")
+
+const findInFiles = require('find-in-files');
+
+const fs = require("fs");
+
+const matches = require('./test.js').config;
+
+const fileList = Object.keys(matches);
+
+const matchingRegex = /i18n=\".+?(?=\")"/gi;
+
+// findInFiles.findSync("i18n=", "../upload/src/app")
+//     .then((results) => {
+//         console.log(results);
+//     });
+
+function processFile(file) {
+    let contents = fs.readFileSync(file, 'utf8');
+
+    // ['metadataService.metadata_descriptions.abstract.tooltip',
+    //     'metadataService.metadata_descriptions.geographic_scope.tooltip']
+    const i18nReferences = contents.match(matchingRegex);
+
+    // for each reference of i18n=
+    for (let ref of i18nReferences) {
+        let addThis = ref.replace('i18n=', 'data-i18n=').replace('@@','');
+        contents = contents.replace(ref, ref + ' ' + addThis);
+    }
+
+    fs.writeFileSync(file, contents);
+}
+
+for(let file of fileList) {
+    processFile(file);
+}
+
+// processFile(fileList[0]);
